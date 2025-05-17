f490ce3178ef9fd7357ca232665a79d39dfe73b4
danielCangianelli <daniel.dw@live.com>
2025-05-16 23:43:08 -0300

    feat implements v1

---
+++ SmsSpamDetector/SmsSpamDetector/SmsSpamDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmsSpamDetectorApp.swift
+//  SmsSpamDetector
+//
+//  Created by Daniel Negreiros Cangianelli on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SmsSpamDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SpamDetectorView()
+        }
+    }
+}
